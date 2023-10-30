d94ec1b71a74063bd71fa8b5c71abbe5373f7931
Steven Castro <castrostevenyc@gmail.com>
2023-10-30 13:56:01 -0400

    update

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -3,13 +3,16 @@ import { authOptions } from "@/app/lib/auth";
 import prisma from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-
+import { URL } from "url";
 // See your keys here: https://dashboard.stripe.com/apikeys
 const stripe = require("stripe")(
   "sk_test_51Kxci9GZqFfSTHOMXLz8jrK2e2uoByeZmo2e5yPcZ1otfBdDwr0aRTwohKI6nE11hW0PlsNwZlqTYy1DGfYXdzpz0057zGZdj1"
 );
 
 export async function POST(request: Request) {
+  const parsedUrl = new URL(request.url);
+  const baseUrl = parsedUrl.origin;
+
   const session = await getServerSession(authOptions);
   if (session && session.user) {
     const prismaUser = await prisma.user.findUnique({
@@ -21,7 +24,7 @@ export async function POST(request: Request) {
       const accountLink = await stripe.accountLinks.create({
         account: prismaUser.stripeAccountId,
         refresh_url: "https://example.com/reauth",
-        return_url: "http://localhost:3000/dashboard/account",
+        return_url: `${baseUrl}/dashboard/account`,
         type: "account_onboarding",
       });
       console.log("ACCOUNTLINK", accountLink);
