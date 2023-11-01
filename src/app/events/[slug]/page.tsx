2f68c3f6132d1f43098b68cb85789e88a702987d
Steven Castro <castrostevenyc@gmail.com>
2023-11-01 01:11:12 -0400

    update

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,3 +1,46 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+import prisma from "@/app/lib/db";
+import Image from "next/image";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const data = await prisma.event.findUnique({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      tickets: {},
+      organizer: {},
+      users: {},
+    },
+  });
+  const numberOfLikes = data?.users.length;
+  if (data) {
+    return (
+      <div>
+        My Post: {JSON.stringify(data)}
+        <ul>
+          <li>Number of Like :{numberOfLikes}</li>
+          <li>Price per ticker: {data.tickets[0].price}</li>
+          <li>Event Name: {data.eventName}</li>
+          <li>Location : {data.location}</li>
+          <li>Hosted by : {data.organizer.name}</li>
+          <li>Description : {data.description}</li>
+          {data.imgUrl && (
+            <li>
+              <Image
+                src={data.imgUrl}
+                alt={data.eventName}
+                width={500}
+                height={500}
+              />
+            </li>
+          )}
+          <form action="">
+            <button type="submit">Buy Ticket</button>
+          </form>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>Error Fetching Data</div>;
+  }
 }
