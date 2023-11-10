f673b6fec0f7e1c63b2d21ea583a32dfa6327e4d
Steven Castro <castrostevenyc@gmail.com>
2023-11-10 12:15:15 -0500

    update

--- src/app/components/CreateEventForm/CreateTicket/index.tsx
+++ src/app/components/CreateEventForm/CreateTicket/index.tsx
@@ -1,89 +1,34 @@
-import { Formik, FormikHelpers } from "formik";
-import { ChangeEvent } from "react";
-
-// Interface the values
-interface Values {
-  price: number;
-}
-interface Errors {
-  price?: string;
-}
-// Set initial Values
-const initialValues = {
-  price: 0,
-};
-// Set the form validation
-const formikFormValidation = (values: Values): Errors => {
-  const errors: Errors = {};
-  if (values.price <= 0) {
-    errors.price = "Price Needs to be greater than 0";
-  }
-
-  return errors;
-};
-
-const submitHandler = (
-  values: Values,
-  { setSubmitting }: FormikHelpers<Values>
-) => {
-  setTimeout(() => {
-    alert(JSON.stringify(values, null, 2));
-    setSubmitting(false);
-  }, 400);
-};
-
 interface CreateTicketProps {
   next: () => void;
   back: () => void;
 }
 
 export const CreateTicket = ({ next, back }: CreateTicketProps) => {
+  const submitHandler = () => {};
   return (
-    <Formik
-      initialValues={initialValues}
-      validate={formikFormValidation}
-      onSubmit={submitHandler}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <div className="form-control w-full max-w-xs">
-            <input
-              className={`input input-bordered w-full max-w-xs ${
-                errors.price && "input-error"
-              }`}
-              type="number"
-              required
-              name="price"
-              placeholder="9.99"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.price > 0 ? values.price : ""}
-            />
-            <label className="label">
-              {errors.price && touched.price && (
-                <span className="label-text-alt text-red-400">
-                  {errors.price}
-                </span>
-              )}
-            </label>
-          </div>
-          <button
-            className="btn btn-primary"
-            type="submit"
-            disabled={isSubmitting}
-          >
+    <form className="flex flex-col gap-2 justify-center items-center">
+      <div className="form-control w-full max-w-xs">
+        <label htmlFor="price">Price</label>
+        <input
+          className={`input input-bordered w-full max-w-xs`}
+          type="number"
+          required
+          name="price"
+          placeholder="9.99"
+        />
+      </div>
+      <div className="flex gap-2">
+        <div>
+          <button className="btn btn-primary" type="submit" onClick={back}>
+            Back
+          </button>
+        </div>
+        <div>
+          <button className="btn btn-primary" type="submit" onClick={next}>
             Submit
           </button>
-        </form>
-      )}
-    </Formik>
+        </div>
+      </div>
+    </form>
   );
 };
