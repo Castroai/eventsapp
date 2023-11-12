fd586e105dde17482ca34e5ed7726896681b0627
Mahmoud <tkhattab1999@gmail.com>
2023-11-11 22:26:01 -0500

    started developing figma events page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { authOptions } from "@/app/lib/auth";
 import prisma from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { ImLocation } from 'react-icons/im';
+import { AiFillLike } from 'react-icons/ai'
 
 export default async function Page({
   params,
@@ -27,40 +29,58 @@ export default async function Page({
       <MainLayout>
         <div className="container mx-auto">
           <ul>
-            <li>Number of Like :{numberOfLikes}</li>
-            {data.tickets.length > 0 && (
-              <li>Price per ticker: {data.tickets[0].price}</li>
-            )}
+            <div className="items-center grid grid-cols-2 gap-4">
+              <div className="col-span-1">
+                <li className="flex font-bold pt-6 text-5xl">{data.eventName}</li>
 
-            <li>Event Name: {data.eventName}</li>
-            <li>Location : {data.location}</li>
-            <li>Hosted by : {data.organizer.name}</li>
-            <li>
-              {data.description && (
-                <div
-                  className="prose"
-                  dangerouslySetInnerHTML={{ __html: data.description }}
-                />
-              )}
-            </li>
-            {data.imgUrl && (
-              <li>
-                <Image
-                  priority
-                  src={data.imgUrl}
-                  alt={data.eventName}
-                  width={500}
-                  height={500}
-                />
-              </li>
-            )}
-            {data.tickets.length > 0 && (
-              <form action="">
-                <button type="submit">
-                  Buy Ticket <li>Price per ticker: {data.tickets[0].price}</li>
-                </button>
-              </form>
-            )}
+                <li className="pb-6 text-sm">By: {data.organizer.name}</li>
+
+                <li className="font-bold text-2xl">About</li>
+                <li className="flex pb-6 gap-11">
+                  {data.description && (
+                    <div
+                      className="prose"
+                      dangerouslySetInnerHTML={{ __html: data.description }}
+                    />
+                  )}
+                </li>
+
+                <li className="font-bold text-2xl">Location</li>
+                <li className="flex items-center gap-3 pb-6"><ImLocation /> {data.location}</li>
+
+                <li className="flex items-center gap-3"><AiFillLike /> {numberOfLikes}</li>
+              </div>
+
+              <div className="col-span-1">
+                {data.imgUrl && (
+                  <li className="pt-12">
+                    <Image
+                      priority
+                      src={data.imgUrl}
+                      alt={data.eventName}
+                      width={500}
+                      height={500}
+                    />
+                  </li>
+                )}
+
+                <li className="flex font-bold pt-6 ">
+                  {data.tickets.length > 0 && (
+                    <li className="badge badge-lg badge-outline gap-6">
+                      ${data.tickets[0].price}
+
+                      {data.tickets.length > 0 && (
+                        <form action="">
+                          <button className="badge bg-gray-200" type="submit">Buy Now</button>
+                        </form>
+                      )}
+                    </li>
+                  )}
+                </li>
+              </div>
+
+              <div className="col-span-1"></div>
+            </div>
           </ul>
           {session?.user && <CommentBox eventId={data.id} />}
           <div>
